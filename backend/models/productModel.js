0beee46f77882a0d68cfcf839df5f0e0ef7c662b
Mamun <mamun.cse63@gmail.com>
2020-10-11 02:57:00 +0600

    Model data

---
+++ backend/models/productModel.js
@@ -0,0 +1,74 @@
+import mongoose from 'mongoose'
+
+// individual
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId, // want to know which user I mean admin created the product
+      required: true,
+      ref: 'User', // as a relationship between product and user
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+
+    reviews: [reviewSchema],
+
+    // average rating
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Product = mongoose.model('Product', productSchema)
+
+export default Product
