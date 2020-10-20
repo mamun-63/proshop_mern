8bf7dbb887f655a6b2d2911add49e21ed0e39f94
Mamun <mamun.cse63@gmail.com>
2020-10-20 17:52:36 +0600

    fix20 21 Data Seeder Script [import sample data to database]

--- backend/data/users.js
+++ backend/data/users.js
@@ -2,19 +2,19 @@ import bcrypt from 'bcryptjs'
 
 const users = [
   {
-    user: 'Admin User',
+    name: 'Admin User',
     email: 'admin@example.com',
     password: bcrypt.hashSync('123456', 10),
     isAdmin: true,
   },
   {
-    user: 'John Doe',
+    name: 'John Doe',
     email: 'john@example.com',
     password: bcrypt.hashSync('123456', 10),
   },
   {
-    user: 'Jane Doe',
-    email: 'jane@example.com',
+    name: 'Jane Doe',
+    email: 'Jane@example.com',
     password: bcrypt.hashSync('123456', 10),
   },
 ]
