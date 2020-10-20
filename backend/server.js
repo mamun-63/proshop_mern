558689ba40c4461d25ff41a2ed605ed37beb9bca
Mamun <mamun.cse63@gmail.com>
2020-10-20 21:06:04 +0600

    22 Fetch products from database

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -14,14 +15,8 @@ app.get('/', (req, res) => {
   res.send('API is running ...')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products) // not actual json, its js array now
-})
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id)
-  res.json(product)
-})
+// mount the express router
+app.use('/api/products', productRoutes)
 
 const PORT = process.env.PORT || 5000
 
